3f01c17f8ac01315783589b5ebbbb76e59bf84ce
mzgcz <j.wenjiao@gmail.com>
2016-10-25 06:33:25 +0800

    add custom

--- init.el
+++ init.el
@@ -16,3 +16,15 @@
 (require 'init-package)
 (require 'init-global)
 (require 'init-python)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(magit-pull-arguments nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
