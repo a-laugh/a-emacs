2c83d8f1f1bdefa4c84f6de549910c56b011d4a6
蒋文佼10175784 <jiang.wenjiao3@zte.com.cn>
2016-10-26 09:54:55 +0800

    package refresh once

--- cells/package/init-package.el
+++ cells/package/init-package.el
@@ -2,9 +2,15 @@
 (package-initialize)
 (add-to-list 'package-archives '("melpa-cn" . "http://elpa.zilongshanren.com/melpa/") t)
 
+(defvar al-package-refreshed nil)
+(defun al-package-refreshed-p ()
+  al-package-refreshed)
+
 (defun al-package-install (package)
   (unless (package-installed-p package)
-    (package-refresh-contents)
+    (unless (al-package-refreshed-p)
+      (package-refresh-contents)
+      (setq al-package-refreshed nil))
     (package-install package)))
 
 (provide 'init-package)
---
+++ cells/python/pkg-4-debian.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+pip install flake8 --upgrade
+pip install jedi --upgrade
+pip install autopep8 --upgrade
