cd470be791c1630f2ef8b1d45c9fb6fd2af32e1d
蒋文佼10175784 <jiang.wenjiao3@zte.com.cn>
2016-10-27 12:55:55 +0800

    error when install package not in archives

--- cells/package/init-package.el
+++ cells/package/init-package.el
@@ -2,22 +2,15 @@
 (package-initialize)
 (add-to-list 'package-archives '("melpa-cn" . "http://elpa.zilongshanren.com/melpa/") t)
 
-;; (defvar al-package-refreshed nil)
-;; (defun al-package-refreshed-p ()
-;;   al-package-refreshed)
-
-;; (defun al-package-install (package)
-;;   (unless (package-installed-p package)
-;;     (unless (al-package-refreshed-p)
-;;       (package-refresh-contents)
-;;       (setq al-package-refreshed nil))
-;;     (package-install package)))
+(defvar al-package-refreshed nil)
+(defun al-package-refreshed-p ()
+  al-package-refreshed)
 
 (defun al-package-install (package)
   (unless (package-installed-p package)
-    (package-install package)
-    (unless (package-installed-p package)
+    (unless (al-package-refreshed-p)
       (package-refresh-contents)
-      (package-install package))))
+      (setq al-package-refreshed nil))
+    (package-install package)))
 
 (provide 'init-package)
--- init.el
+++ init.el
@@ -3,4 +3,3 @@
 
 (add-to-list 'load-path "~/.emacs.d/cells/")
 (require 'init-cells)
-
