1ef9489474a1f6f6a7ac48ea6675e941c7d00274
mzgcz <mzgcz@self-sys>
2016-11-16 22:33:09 +0800

    split custom file

--- init.el
+++ init.el
@@ -1,2 +1,7 @@
 (add-to-list 'load-path "~/.emacs.d/cells/")
 (require 'init-cells)
+
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(when (file-exists-p custom-file)
+  (load custom-file))
+
