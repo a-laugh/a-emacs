e24604bb981132e8044f88ca7eca6dcf9cc1640e
mzgcz <mzgcz@self-sys>
2016-11-16 23:15:08 +0800

    split common module

---
+++ common/init-common.el
@@ -0,0 +1,5 @@
+(add-to-list 'load-path (expand-file-name "package"
+					  (expand-file-name "common" user-emacs-directory)))
+(require 'init-package)
+
+(provide 'init-common)
--- init.el
+++ init.el
@@ -1,7 +1,9 @@
-(add-to-list 'load-path "~/.emacs.d/cells/")
+(add-to-list 'load-path (expand-file-name "common" user-emacs-directory))
+(require 'init-common)
+
+(add-to-list 'load-path (expand-file-name "cells" user-emacs-directory))
 (require 'init-cells)
 
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 (when (file-exists-p custom-file)
   (load custom-file))
-
