1143e4aaefbd518cd06232387e9330665b644369
mzgcz <mzgcz@self-sys>
2016-11-26 22:23:59 +0800

    configure org key

--- cells/org/init-org.el
+++ cells/org/init-org.el
@@ -1,6 +1,8 @@
 (require 'org)
-(global-set-key (kbd "C-c a") 'org-agenda)
 (global-set-key (kbd "C-c l") 'org-store-link)
+(global-set-key (kbd "C-c a") 'org-agenda)
+(global-set-key (kbd "C-c c") 'org-capture)
+(global-set-key (kbd "C-c b") 'org-iswitchb)
 (setq org-log-done t)
 (setq org-startup-indented t)
 
