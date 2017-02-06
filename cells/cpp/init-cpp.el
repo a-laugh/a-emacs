bfb9a077d94a40a979d9b690c063a84b4a1b46c6
蒋文佼10175784 <jiang.wenjiao3@zte.com.cn>
2017-02-06 16:03:07 +0800

    use company-gtags

--- cells/cpp/init-cpp.el
+++ cells/cpp/init-cpp.el
@@ -6,10 +6,10 @@
 (with-eval-after-load 'counsel-gtags
   (define-key counsel-gtags-mode-map (kbd "M-t") 'counsel-gtags-find-definition)
   (define-key counsel-gtags-mode-map (kbd "M-r") 'counsel-gtags-find-reference)
-  (define-key counsel-gtags-mode-map (kbd "M-s") 'counsel-gtags-find-symbol))
+  (define-key counsel-gtags-mode-map (kbd "M-s") 'counsel-gtags-find-symbol)
+  (define-key counsel-gtags-mode-map (kbd "s-,") 'company-gtags))
 
 (setenv "GTAGSLIBPATH" "/usr/include")
 (setenv "MAKEOBJDIRPREFIX" (file-truename "~/play-exam/life"))
-(setq company-backends '((company-dabbrev-code company-gtags)))
 
 (provide 'init-cpp)
