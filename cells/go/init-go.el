0013f5a0a600b782d9b7b6f6e34383347bf38e6b
蒋文佼10175784 <jiang.wenjiao3@zte.com.cn>
2017-03-18 16:52:31 +0800

    support go env

---
+++ cells/go/init-go.el
@@ -0,0 +1,18 @@
+(al-package-install 'go-mode)
+
+(add-hook 'before-save-hook 'gofmt-before-save)
+(add-hook 'go-mode-hook (lambda()
+			  (local-set-key (kbd "C-c C-r") 'go-remove-unused-imports)))
+
+(add-to-list 'load-path "~/go/src/github.com/dougm/goflymake")
+(require 'go-flycheck)
+(add-hook 'go-mode-hook 'flycheck-mode)
+
+(al-package-install 'auto-complete)
+(al-package-install 'go-autocomplete)
+(ac-config-default)
+(require 'go-autocomplete)
+(require 'auto-complete-config)
+(ac-config-default)
+
+(provide 'init-go)
